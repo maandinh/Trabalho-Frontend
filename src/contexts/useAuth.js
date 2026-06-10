66151c9076e8ee1a45dbf6fdeeceebe1996a3791
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2026-06-09 21:57:13 -0300

    feat: adicionando imagem de qrcode para simular PIX

--- src/contexts/useAuth.js
+++ src/contexts/useAuth.js
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { AuthContext } from "./AuthContext";
 
 export function useAuth() {
-  const context = useContext(AuthContext);
+  return useContext(AuthContext);
   if (!context) {
     throw new Error("useAuth deve ser usado dentro de AuthProvider");
   }
   return context;
 }
+
+export default useAuth;
