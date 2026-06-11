0e92f11d13bfd2cffdd711d45d21b4f670f9585c
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2026-06-11 01:13:11 -0300

    feat: arrumando return do useAuth()

--- src/contexts/useAuth.js
+++ src/contexts/useAuth.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "./AuthContext";
 
 export function useAuth() {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
   if (!context) {
     throw new Error("useAuth deve ser usado dentro de AuthProvider");
   }
